39976c251888197cfe3c09892c3713735e01d250
Dave Charm Bulaquena <i_davecharm.bulaquena@stratpoint.com>
2024-06-13 18:21:18 +0800

    added: cores headers

--- my_openai_project/settings.py
+++ my_openai_project/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
@@ -57,6 +58,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware'
 ]
 
 ROOT_URLCONF = 'my_openai_project.urls'
@@ -78,6 +81,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'my_openai_project.wsgi.application'
+CORS_ALLOW_ALL_ORIGINS = True
 
 
 # Database
@@ -89,6 +93,11 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+CORS_ALLOWED_ORIGINS = [
+    'https://your-frontend-domain.com',
+    'http://localhost:3000',  # For local development (if needed)
+]
 
 
 # Password validation
